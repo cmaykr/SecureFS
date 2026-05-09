cd671ebeadbf81e0a9a83f0682a86a26ba871150
cmaykr <cmaykr@proton.me>
2026-05-09 15:07:03 +0200

    Adds command handler

--- src/main.cpp
+++ src/main.cpp
@@ -9,30 +9,32 @@
 #include <algorithm>
 #include <iterator>
 #include <sstream>
+#include <map>
+
 
 User loginUser(CredentialStore* cs)
 {
     std::string username{};
     std::string password{};
-
+    
     std::cout << "Enter your username: ";
     std::cin >> username;
     std::cout << "Enter your password: ";
     std::cin >> password;
 
     User authUser = cs->authenticateUser(User{username, password, 0, 0});
-
+    
     return authUser;
 }
 
 void createUserMenu(CredentialStore* cs)
 {
-
+    
     std::string username{};
     std::string password{};
     unsigned int securityLevel{};
     unsigned int securityDomain{};
-
+    
     std::cout << "Type in username and password:" << std::endl;
     std::cout << "Username: ";
     std::cin >> username;
@@ -42,7 +44,7 @@ void createUserMenu(CredentialStore* cs)
     std::cin >> securityLevel;
     std::cout << "Security domain: ";
     std::cin >> securityDomain;
-
+    
     User user{username,password, securityLevel, securityDomain};
     cs->addUser(user);
 }
@@ -57,52 +59,34 @@ std::vector<std::string> tokenizer(std::string const& command)
     {
         tokens.push_back(token);
     }
-
-
+    
+    
     return tokens;
 }
 
+void login(int)
+{
+    
+}
+
+static std::map<std::string, void(*)(int)> commandFunctionPointers{{"login", login}};
+
 int main()
 {
-    // User user1{"john123", "", "1234", 3, 2};
-    // User user2{"erik321", "", "password123", 1, 0};
+    commandFunctionPointers.at("login")(0);
     CredentialStore cs{};
-    // cs.addUser(user1);
-    // cs.addUser(user2);
 
-    // SecureFileSystem fs{};
-    // AuthorizationHandler ah{&cs};
-
-    // UserAccessHandler sah{&fs, &ah};
-
-    // std::string text{"Hello!!!"};
-    // // std::cout << text << std::endl;
-
-    // File file{"test.txt", 2, 0, NULL, 0};
-
-    // fs.createFile(file, text.c_str());
-
-    // // std::cout << fs.readFile(file) << std::endl;
-
-    // std::vector<char> objData {sah.getObject(user1, &file)};
-
-    // std::copy(std::begin(objData), std::end(objData), std::ostream_iterator<char>(std::cout));
-
-
-
-    
     User currentUser{};
     bool exit = false;
     std::cout << "Welcome to the secure file system handler!" << std::endl;
     while (exit == false)
     {
         std::string command{};
+        std::cout << "> ";
         getline(std::cin, command, '\n');
 
         std::vector<std::string> commandTokens {tokenizer(command)};
 
-        std::copy(std::begin(commandTokens), std::end(commandTokens), std::ostream_iterator<std::string>(std::cout, " "));
-        std::cout << std::endl;
 
         if (command == "login")
         {
@@ -114,7 +98,7 @@ int main()
         }
         else
         {
-            std::cout << "Invalid command!" << std::endl;
+            std::cout << "Unknown command!" << std::endl;
         }
 
         
